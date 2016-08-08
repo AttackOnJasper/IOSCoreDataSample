b3ddf3b6e0e4a3af83b5b66b049d1b63eec52a11
Jasper Wang <wangdc2010@gmail.com>
2016-08-08 16:09:06 -0400

    table view and 'add' UIBarButton

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -8,10 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource   {
+    @IBAction func addAction(sender: AnyObject) {
+        let alert = UIAlertController(title: "New Thing To Do",
+                                      message: "What needs to do today",
+                                      preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .Default,
+                                       handler: { (action:UIAlertAction) -> Void in
+                                        
+                                        let textField = alert.textFields!.first
+                                        self.actions.append(textField!.text!)
+                                        self.tableView.reloadData()
+        })
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .Default) { (action: UIAlertAction) -> Void in
+        }
+        
+        alert.addTextFieldWithConfigurationHandler {
+            (textField: UITextField) -> Void in
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert,
+                              animated: true,
+                              completion: nil)
+    }
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var actions = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "To Do List"
+        tableView.registerClass(UITableViewCell.self,forCellReuseIdentifier: "Cell")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +55,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    // MARK: - UITableViewDataSource
+    func tableView(tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return actions.count
+    }
+    
+    func tableView(tableView: UITableView,
+                   cellForRowAtIndexPath
+        indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell =
+            tableView.dequeueReusableCellWithIdentifier("Cell")
+        
+        cell!.textLabel!.text = actions[indexPath.row]
+        
+        return cell!
+    }
 
 }
 
